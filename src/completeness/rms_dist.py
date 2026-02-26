ab395d564c16f74ffa9640ce338b6d3c060bf5ab
Ashley Parr <amparr83@gmail.com>
2026-02-26 15:11:40 +0000

    Add logging to RMSDistribution and add the percentage threshold as a config option

--- src/completeness/rms_dist.py
+++ src/completeness/rms_dist.py
@@ -1,7 +1,12 @@
-import image_downloading.hardcastle_catalogue as hdc
 import numpy as np
 from scipy.stats import rv_histogram
 import matplotlib.pyplot as plt
+
+import logging
+import utils.logging
+import image_downloading.hardcastle_catalogue as hdc
+import configparser
+import utils.paths as pths
 
 class RMSDistribution:
     """
@@ -11,6 +16,19 @@ class RMSDistribution:
     """
 
     def __init__(self):
+        self.logger = utils.logging.get_logger("RMSDistribution", logging.DEBUG)
+
+        # Read parameters from the config.ini file
+        config = configparser.ConfigParser()
+        config.read(pths.PROGRAM_CONFIG)
+
+        # we are using sources generated in a loguniform way
+        de_config = config['DEFAULT']
+
+        # Get values from config
+        self.percentage_threshold = float(de_config['RMS_PERCENTAGE_THRESHOLD'])
+
+        # Get the RMS values from the Hardcastle catalogue and filter out NaN values
         self.rms_values = self.get_rms_values()
 
         # Fit a histogram to the RMS values and create a random variable distribution
@@ -20,18 +38,21 @@ class RMSDistribution:
     def get_rms_values(self) -> np.ndarray:
         # Get all RMS values from the catalogue
         catalogue = hdc.HardcastleCatalogue()
+
+        self.logger.info("Extracting RMS values from the Hardcastle catalogue...")
         rms_values = catalogue.get_values(hdc.Source.RMS)
 
         #  Remove NaN values from the RMS data
+        self.logger.info("Filtering any NaNs...")
         rms_values = np.array(rms_values)
         rms_values = rms_values[~np.isnan(rms_values)]
 
         # Choose a threshold to contain a certain percentage of the data, e.g., 99.7% for a normal distribution (3 sigma)
         # This filters from the highest first; that is, removes the largest RMS values until the desired percentage of the data is retained
-        percentage_threshold = 0.95
-        upper_limit = np.percentile(rms_values, percentage_threshold * 100)
+        upper_limit = np.percentile(rms_values, self.percentage_threshold * 100)
 
         # Very few values above this range, but they affect our histogram fitting, so filtering
+        self.logger.info(f"Filtering RMS values to retain {self.percentage_threshold * 100}% of the data...")
         rms_values = rms_values[rms_values < upper_limit]
 
         return rms_values
@@ -49,6 +70,7 @@ class RMSDistribution:
         """
         Plots the histogram of RMS values to visualize the distribution.
         """
+        self.logger.info("Plotting the histogram of RMS values...")
         plt.hist(self.rms_values, bins=100, density=False)
         plt.xlabel('RMS Value (mJy/beam)')
         plt.ylabel('Density')
@@ -73,12 +95,12 @@ class RMSDistribution:
             }
         }
 
-        print("RMS Statistics:")
-        print(f"Mean: {stats['mean']:.4f} mJy/beam")
-        print(f"Median: {stats['median']:.4f} mJy/beam")
-        print(f"Standard Deviation: {stats['std_dev']:.4f} mJy/beam")
-        print(f"90th Percentile: {stats['percentiles']['90th']:.4f} mJy/beam")
-        print(f"95th Percentile: {stats['percentiles']['95th']:.4f} mJy/beam")
+        self.logger.info("RMS Statistics:")
+        self.logger.info(f"Mean: {stats['mean']:.4f} mJy/beam")
+        self.logger.info(f"Median: {stats['median']:.4f} mJy/beam")
+        self.logger.info(f"Standard Deviation: {stats['std_dev']:.4f} mJy/beam")
+        self.logger.info(f"90th Percentile: {stats['percentiles']['90th']:.4f} mJy/beam")
+        self.logger.info(f"95th Percentile: {stats['percentiles']['95th']:.4f} mJy/beam")
         
         return stats
 
