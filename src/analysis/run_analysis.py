502b2dfb37702c779265d2ee3cb0b0a24e3732bd
Solalunara <greenberg04@gmail.com>
2026-02-26 15:21:31 +0000

    hardcastle slurm analysis subdirectory

--- src/analysis/run_analysis.py
+++ src/analysis/run_analysis.py
@@ -1,13 +1,27 @@
 import utils.paths as pth
 from analysis.image_analyzer import ImageAnalyzer
 from utils.logging import get_logger
+import sys
+import argparse
 
 logger = get_logger( __name__ )
 
-def analyze():
-    for subdir in pth.SUBDIRS:
+def analyze( subdirs: list[ str ], fits_input_dir: str | None = None ):
+    for subdir in subdirs:
+        if fits_input_dir is None:
+            fits_input_dir = pth.FITS_PARENT
+
         analyzer = ImageAnalyzer( subdir, export_images=[ 'gaus_model', 'gaus_resid' ], catalog_format='fits' )
         analyzer.analyze_all_FITS_in_input()
 
 if __name__ == '__main__':
-    analyze()
+    parser = argparse.ArgumentParser( prog='python run_analysis.py', 
+                                      description='A program to analyze fits files with PyBDSF' )
+    parser.add_argument( "--input-dir", help="FITS input directory, default utils.paths.FITS_PARENT", type=str, default=None )
+    parser.add_argument( "SUBDIRS", help="Any number of subdirectories, or 0 to use utils.paths.SUBDIRS", nargs='*' )
+    args = parser.parse_args()
+
+    if len( args.SUBDIRS ) > 1:
+        analyze( args.SUBDIRS, args.input_dir )
+    else:
+        analyze( pth.SUBDIRS )
