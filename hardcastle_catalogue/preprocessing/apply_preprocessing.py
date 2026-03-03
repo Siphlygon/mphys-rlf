8757a6cb0094a28fd37378121c4ce48816a488d4
Ashley Parr <amparr83@gmail.com>
2026-03-03 14:54:08 +0000

    Working more on implementing pre-processing

--- hardcastle_catalogue/preprocessing/apply_preprocessing.py
+++ hardcastle_catalogue/preprocessing/apply_preprocessing.py
@@ -1,11 +1,13 @@
-import logging
-import utils.logging
-import utils.paths as pths
 from astropy.io import fits
 from tqdm import tqdm
 import numpy as np
 from pathlib import Path
 import pandas as pd
+from astropy.stats import sigma_clipped_stats
+import logging
+
+import utils.logging
+import utils.paths as pths
 
 class CutoutPreprocessor:
 
@@ -18,7 +20,7 @@ class CutoutPreprocessor:
         Loads the initial dataset from a FITS file into a pandas dataframe for future use.
 
         :param dataset_file_path: The path to the FITS file containing the initial dataset with header information and pixel values.
-        :return:
+        :return: A pandas DataFrame containing the initial dataset with header information and pixel values.
         """
         self.logger.info("Loading Hardcastle catalogue from FITS file...")
 
@@ -40,14 +42,19 @@ class CutoutPreprocessor:
                     self.logger.error(f"Error loading Hardcastle catalogue item {idx}: {e}")
                     catalogue_data.append({'index': idx, 'pixel_values': np.nan})
 
-        # Initialise all other columns to False right now
-        catalogue_data = [{**item, 'broken': False, 'bad_S/N': False, 'edge_pixels': False, 'incomplete': False} for item in catalogue_data]
+        # Initialise all other columns to default right now
+        catalogue_data = [{**item, 'broken': False, 'S/N_sigma': 0, 'edge_pixels': 0, 'incomplete': False} for item in catalogue_data]
 
         # Set up DataFrame columns
-        columns = ['index', 'pixel_values', 'broken', 'bad_S/N', 'edge_pixels', 'incomplete']  # Add more columns as needed for header information
+        columns = ['index', 'pixel_values', 'broken', 'S/N_sigma', 'edge_pixels', 'incomplete']  # Add more columns as needed for header information
         dataset = pd.DataFrame(catalogue_data, columns=columns)
 
         return dataset
+
+    # ---------- THRESHOLDS ----------
+
+
+    # ---------- FLAGS ----------
 
     def identify_broken_source(self, image : np.ndarray) -> bool:
         # The criterion for broken source they state in the paper is NaN values or blank image values. The actual
@@ -56,9 +63,36 @@ class CutoutPreprocessor:
 
         return np.isnan(image).any() or (image == 0).all()
 
-    def identify_bad_SNR(self, image):
-        #todo: instead of a bool flag, would be better for diagnostics to just store SNR calc'd
-        return
+
+    """
+    Code below modified from the original LOFAR-diffusion repository, found here:
+    https://github.com/tmartinezML/LOFAR-Diffusion/blob/develop/src/data/image_utils.py
+    """
+    def calculate_SNR_sigma(self,
+                            image : np.ndarray,
+                            threshold : float = 5) -> float:
+        """
+        Identifies a source and background region based on a threshold with the median pixel value in a region and the
+        standard deviation of the pixel values in that region. The S/N is then calculated as the the ratio of average
+        pixel values in both regions.
+
+        :param image:
+        :param threshold:
+        :return:
+        """
+        _, median, stddev = sigma_clipped_stats(image)
+        threshold = median + threshold * stddev
+        mask = image > threshold
+
+        if mask.sum() == 0:
+            return self.calculate_SNR_sigma(image, threshold - 0.5)
+
+        if image[~mask].sum() == 0:
+            print("No pixels below threshold.")
+            return -1
+
+        return image[mask].sum() / image[~mask].sum() * (~mask).sum() / mask.sum()
+
 
     def identify_edge_pixels(self, image):
         #todo: similarly, instead of bool store the brightest edge pixel as frac brightness or smth
@@ -67,33 +101,44 @@ class CutoutPreprocessor:
     def identify_incomplete_images(self, image):
         return
 
-    def compute_flags(self, image : np.ndarray) -> dict:
-        return {
-            'broken': self.identify_broken_source(image),
-            'bad_S/N': self.identify_bad_SNR(image),
-            'edge_pixels': self.identify_edge_pixels(image),
-            'incomplete': self.identify_incomplete_images(image)
-        }
+    # ---------- MAIN PROCESSING ----------
+
+    def compute_flags(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        """
+        Compute the flags for each image in the dataset and overwrite the dataset with the new flags. This will be used
+        to filter the dataset in the next step.
+
+        :param dataset: The dataset containing the pixel values and other information for each source.
+        :return: The dataset with the new flags computed.
+        """
+        broken = []
+        snr_sigma = []
+        edge_pixels = []
+
+        # Compute flags for each image
+        for arr in dataset["pixel_values"]:
+            broken.append(self.identify_broken_source(arr))
+            snr_sigma.append(self.calculate_SNR_sigma(arr))
+            edge_pixels.append(self.identify_edge_pixels(arr))
+
+        # Apply flags to the dataset
+        dataset["broken"] = broken
+        dataset["S/N_sigma"] = snr_sigma
+        dataset["edge_pixels"] = edge_pixels
+
+        return dataset
 
     def apply_preprocessing(self):
-        # 0. - they seem to have a step 0 where items w/ problems are identified ahead of time
-        # 1. LAS (??) threshold - largest angular size. Note it looks like they may have obtained their images from mosaics
-        # 2. Flux threshold
-        # 3. SNR threshold
-        # 4. Edge pixel threshold
-        # 5. Broken images
-        # 6. Filter dataset
         dataset = self.load_initial_dataset()
 
-        for image in tqdm(dataset['pixel_values']):
-            self.identify_broken_sources(image)
-            self.identify_bad_SNR(image)
-            self.ientify_edge_pixels(image)
-            self.identify_incomplete_images(image)
+        # Compute the flags for each image in the dataset
+        self.compute_flags(dataset)
 
+        # Filter the dataset based on the flags
+        # todo: not all flags are going to be boolean
         clean_dataset = dataset[
             (~dataset["broken"]) &
-            (~dataset["bad_S/N"]) &
+            (~dataset["S/N_sigma"]) &
             (~dataset["edge_pixels"]) &
             (~dataset["incomplete"])
         ]
