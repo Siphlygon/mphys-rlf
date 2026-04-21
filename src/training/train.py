1daf11e68f8dfa00c5de40485237184d4db83c46
Solalunara <greenberg04@gmail.com>
2026-04-21 15:52:09 +0100

    more ddp attempts

--- src/training/train.py
+++ src/training/train.py
@@ -6,12 +6,27 @@ from training.trainer import DiffusionTrainer
 import torch.distributed as dist
 import torch.multiprocessing as mp
 import os
+import torch
 
+## gpt code ##
+def get_default_backend_for_device(device):
+    if isinstance(device, str):
+        device = torch.device(device)
 
-if __name__ == "__main__":
-    # Limit visible GPUs if you want:
-    #device_utils.set_visible_devices(1)
-    world_size = int(os.environ["SLURM_NTASKS"])
+    if device.type == "cuda":
+        return "nccl"
+    elif device.type == "cpu":
+        return "gloo"
+    else:
+        raise ValueError(f"Unsupported device type: {device.type}")
+##############
+
+def train_model_ddp( rank, world_size ):
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '12365'
+    acc = "cuda" if torch.cuda.is_available() else "cpu"
+    backend = get_default_backend_for_device(acc)
+    dist.init_process_group(backend, init_method="tcp://127.0.0.1:12365", rank=rank, world_size=world_size)
 
     # Set model preset:
     # (i.e. name of the json file in the model_configs directory)
@@ -20,25 +35,8 @@ if __name__ == "__main__":
     # Hyperparameters
     conf = ModelConfig.from_preset(model_preset)
 
-    # Change the name if you want:
-    # (otherwise default name is used)
-    # conf.model_name = "Alternative_Name"
-
     # Load dataset:
     dataset = datasets.ImagePathDataset( "hardcastle_catalogue/clean_hardcastle_catalogue.hdf5" )
-
-    trainer = DiffusionTrainer(config=conf, dataset=dataset)
-
-    mp.spawn( train_model_ddp,
-              args=(world_size,),
-              nprocs=world_size,
-              join=True )
-
-
-def trian_model_ddp( rank, world_size ):
-    acc = torch.accelerator.current_accelerator()
-    backend = torch.distributed.get_default_backend_for_device(acc)
-    dist.init_process_group(backend, rank=rank, world_size=world_size)
 
     # Initialize trainer
     trainer = DiffusionTrainer( rank=rank, world_size=world_size, config=conf, dataset=dataset)
@@ -47,3 +45,25 @@ def trian_model_ddp( rank, world_size ):
     trainer.training_loop()
 
     dist.destroy_process_group()
+
+
+
+if __name__ == "__main__":
+    # Limit visible GPUs if you want:
+    #device_utils.set_visible_devices(1)
+    try:
+        world_size = int(os.environ["SLURM_NTASKS"])
+    except:
+        world_size = 1
+
+
+    # Change the name if you want:
+    # (otherwise default name is used)
+    # conf.model_name = "Alternative_Name"
+
+    mp.spawn( train_model_ddp,
+              args=(world_size,),
+              nprocs=world_size,
+              join=True )
+
+
