583646a0d782c39cc492789c611e49dc54b71430
Solalunara <greenberg04@gmail.com>
2026-04-28 13:56:20 +0100

    sampling retrained model

--- src/program_prep/download_models.py
+++ src/program_prep/download_models.py
@@ -5,12 +5,12 @@ import shutil
 from utils.distributed import DistributedUtils
 
 def download_models():
-    models = [ pth.PRETRAINED_PARENT / f'parameters_{name}_model.pt' for name in pth.MODEL_NAMES ]
+    models = [ pth.PRETRAINED_PARENT / f'parameters_{name}.pt' for name in pth.MODEL_NAMES ]
 
     # dict to make sure ordering is correct
     links_dict = {
-        "LOFAR": "https://cloud.hs.uni-hamburg.de/s/KTAFWFnLByMgNRn", 
-        "FIRST": "https://cloud.hs.uni-hamburg.de/s/xs7bbt99AMFf8gP"
+        "LOFAR_model": "https://cloud.hs.uni-hamburg.de/s/KTAFWFnLByMgNRn", 
+        "FIRST_model": "https://cloud.hs.uni-hamburg.de/s/xs7bbt99AMFf8gP"
     }
     links = [ links_dict[ name ] for name in pth.MODEL_NAMES ]
 
@@ -21,7 +21,7 @@ def download_models():
             print("Done.")
 
         # Copy the model to the sampling directory if it doesn't exist
-        sampling_file = pth.MODEL_PARENT / f"{name}_model" / f"parameters_{name}_model.pt"
+        sampling_file = pth.MODEL_PARENT / f"{name}" / f"parameters_{name}.pt"
         if not sampling_file.exists():
             shutil.copyfile( model, sampling_file )
 
