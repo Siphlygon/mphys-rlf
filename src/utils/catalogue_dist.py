Add RedshiftDistribution class, allow child classes to specify whether to use resolved items or not, and fix bug in units for statistics print out

src/utils/catalogue_dist.py
@@ -19,14 +19,17 @@ class CatalogueDistribution:
                  logger_name: str = "CatalogueDistribution",
                  bin_num: int = 100,
                  value_name: str = "Value",
-                 units: str = "Units"):
+                 units: str = "Units",
+                 resolved_only: bool = True):
         """
         Initializes the CatalogueDistribution.
 
         Args:
+            logger_name (str, optional): The name of the logger to use for this class. Defaults to "CatalogueDistribution".
             bin_num (int, optional): The number of bins for the histogram. Defaults to 100.
             value_name (str, optional): The name of the value being represented. Defaults to "Value".
             units (str, optional): The units of the value being represented. Defaults to "Units".
+            resolved_only (bool, optional): Whether to consider only resolved sources. Defaults to True.
         """
         self.logger = utils.logging.get_logger(logger_name, logging.DEBUG)
         
@@ -35,7 +38,7 @@ class CatalogueDistribution:
         self.units = units
 
         # Get the RMS values from the Hardcastle catalogue and filter out NaN values
-        self.catalogue = HardcastleCatalogue()
+        self.catalogue = HardcastleCatalogue(resolved_only=resolved_only)
         self.values = self.get_values()
 
         # Fit a histogram to the RMS values and create a random variable distribution
@@ -96,10 +99,10 @@ class CatalogueDistribution:
         :param show_output: Whether to display the statistics in the logger.
         :return: A dictionary containing the mean, median, standard deviation, and percentiles of the values.
         """
-        if value_name is not None:
-            self.value_name = value_name
-        if units is not None:
-            self.units = units
+        if value_name is None:
+            value_name = self.value_name
+        if units is None:
+            units = self.units
 
         stats = {
             'mean': np.mean(self.values),
@@ -130,14 +133,13 @@ class RMSDistribution(CatalogueDistribution):
     fits a histogram to the RMS values and creates a random variable distribution that can be used to generate new RMS
     values that follow the same distribution as the original data.
     """
-    def __init__(self, bin_num: int = 100):
+    def __init__(self, bin_num: int = 100, resolved_only: bool = True):
         """
         Initializes the RMSDistribution by extracting RMS values from the Hardcastle catalogue, fitting a histogram to these values, and creating a random variable distribution.
 
         Args:
             bin_num (int, optional): The number of bins for the histogram. Defaults to 100.
-            value_name (str, optional): The name of the value being analyzed. Defaults to "RMS".
-            units (str, optional): The units of the value being analyzed. Defaults to "mJy/beam".
+            resolved_only (bool, optional): Whether to consider only resolved sources. Defaults to True.
         """
         # Read parameters from the config.ini file
         config = configparser.ConfigParser()
@@ -149,7 +151,7 @@ class RMSDistribution(CatalogueDistribution):
         # Get values from config
         self.percentage_threshold = float(de_config['RMS_PERCENTAGE_THRESHOLD'])
         
-        super().__init__("RMSDistribution", bin_num, "RMS", "mJy/beam")
+        super().__init__("RMSDistribution", bin_num, "RMS", "mJy/beam", resolved_only=resolved_only)
        
     def get_values(self) -> np.ndarray:
         """
@@ -180,7 +182,14 @@ class LASDistribution(CatalogueDistribution):
     fits a histogram to the LAS values and creates a random variable distribution that can be used to generate new LAS
     values that follow the same distribution as the original data.
     """
-    def __init__(self, bin_num: int = 100):
+    def __init__(self, bin_num: int = 100, resolved_only: bool = True):
+        """Initializes the LASDistribution by extracting LAS values from the Hardcastle catalogue, filtering out NaN and zero values, applying a threshold to retain a certain percentage of the data, fitting a histogram to these values, and creating a random variable distribution.
+
+        Args:
+            bin_num (int, optional): _description_. Defaults to 100.
+            resolved_only (bool, optional): _description_. Defaults to True.
+        """
+        
         # # Read parameters from the config.ini file
         # config = configparser.ConfigParser()
         # config.read(pths.PROGRAM_CONFIG)
@@ -193,7 +202,7 @@ class LASDistribution(CatalogueDistribution):
         # self.percentage_threshold = float(de_config['RMS_PERCENTAGE_THRESHOLD'])
         self.percentage_threshold = 0.98
         
-        super().__init__("LASDistribution", bin_num, "LAS", "arcseconds")
+        super().__init__("LASDistribution", bin_num, "LAS", "arcseconds", resolved_only=resolved_only)
     
     def get_values(self) -> np.ndarray:
         """
@@ -227,7 +236,14 @@ class PeakFluxDistribution(CatalogueDistribution):
     fits a histogram to the peak flux values and creates a random variable distribution that can be used to generate new peak flux
     values that follow the same distribution as the original data.
     """
-    def __init__(self, bin_num: int = 100):
+    def __init__(self, bin_num: int = 100, resolved_only: bool = True):
+        """Initializes the PeakFluxDistribution by extracting peak flux values from the Hardcastle catalogue, filtering out NaN values, applying a threshold to retain a certain percentage of the data, fitting a histogram to these values, and creating a random variable distribution.
+
+        Args:
+            bin_num (int, optional): The number of bins for the histogram. Defaults to 100.
+            resolved_only (bool, optional): Whether to consider only resolved sources. Defaults to True.
+        """
+        
         # Get values from config
         # self.percentage_threshold = float(de_config['PEAK_FLUX_PERCENTAGE_THRESHOLD'])
         self.percentage_threshold = 0.80
@@ -235,7 +251,7 @@ class PeakFluxDistribution(CatalogueDistribution):
         # todo: consider having fits to a log distribution instead, as flux values are most definitely lognormally distributed
         # todo: which results in the rv histogram being very poor
 
-        super().__init__("PeakFluxDistribution", bin_num, "Peak Flux", "mJy/beam")
+        super().__init__("PeakFluxDistribution", bin_num, "Peak Flux", "mJy/beam", resolved_only=resolved_only)
 
     def get_values(self) -> np.ndarray:
         """
@@ -267,6 +283,12 @@ class PeakPixDistribution(CatalogueDistribution):
     values that follow the same distribution as the original data.
     """
     def __init__(self, bin_num: int = 100, path: Path | None = None):
+        """Initializes the PeakPixDistribution by extracting peak pixel values from cutout images of the Hardcastle catalogue, applying a threshold to retain a certain percentage of the data, fitting a histogram to these values, and creating a random variable distribution.
+
+        Args:
+            bin_num (int, optional): The number of bins for the histogram. Defaults to 100.
+            path (Path | None, optional): The path to the preprocessed data file containing the cutout images. If None, a default path is used. Defaults to None.
+        """
         if path is None:
             self.path = pths.DATASET_PARENT / "clean_hardcastle_catalogue.h5"
         else:
@@ -311,18 +333,64 @@ class PeakPixDistribution(CatalogueDistribution):
 
         return peak_pixel_values
 
+
+class RedshiftDistribution(CatalogueDistribution):
+    """
+    A class to model the distribution of redshift values of resolved sources from the Hardcastle catalogue. It
+    fits a histogram to the redshift values and creates a random variable distribution that can be used to generate new redshift
+    values that follow the same distribution as the original data.
+    """
+    def __init__(self, bin_num: int = 100, resolved_only: bool = True):
+        """Initializes the RedshiftDistribution by extracting redshift values from the Hardcastle catalogue, filtering out NaN values, applying a threshold to retain a certain percentage of the data, fitting a histogram to these values, and creating a random variable distribution.
+
+        Args:
+            bin_num (int, optional): The number of bins for the histogram. Defaults to 100.
+            resolved_only (bool, optional): Whether to consider only resolved sources. Defaults to True.
+        """
+        
+        # Get values from config
+        # self.percentage_threshold = float(de_config['REDSHIFT_PERCENTAGE_THRESHOLD'])
+        self.percentage_threshold = 0.98
+        
+        super().__init__("RedshiftDistribution", bin_num, "Redshift", "z", resolved_only=resolved_only)
+    
+    def get_values(self) -> np.ndarray:
+        """
+        Gets the redshift values from the Hardcastle catalogue, filters out NaN values, and applies a threshold to retain a certain percentage of the data.
+
+        Returns:
+            np.ndarray: An array of redshift values that have been filtered to retain a specified percentage of the data.
+        """
+        # Get all redshift values from the catalogue
+        self.logger.info("Extracting redshift values from the Hardcastle catalogue...")
+        redshift_values = self.catalogue.get_values(Source.Redshift)
+
+        #  Remove NaN values from the redshift data
+        redshift_values = np.array(redshift_values)
+        redshift_values = redshift_values[~np.isnan(redshift_values)]
+
+        # Filter out extreme redshift values to focus on the main distribution, based on the specified percentage threshold
+        upper_limit = np.percentile(redshift_values, self.percentage_threshold * 100)
+        self.logger.info(f"Filtering redshift values to retain {self.percentage_threshold * 100}% of the data...")
+        redshift_values = redshift_values[redshift_values < upper_limit]
+
+        return redshift_values
+
 if __name__ == "__main__":
     rms_dist = RMSDistribution()
     las_dist = LASDistribution()
     peak_flux_dist = PeakFluxDistribution()
     peak_pixel_dist = PeakPixDistribution()
+    redshift_dist = RedshiftDistribution()
 
     rms_dist.plot()
     las_dist.plot()
     peak_flux_dist.plot()
     peak_pixel_dist.plot()
+    redshift_dist.plot()
 
     rms_stats = rms_dist.get_statistics(show_output=True)
     las_stats = las_dist.get_statistics(show_output=True)
     peak_flux_stats = peak_flux_dist.get_statistics(show_output=True)
     peak_pixel_stats = peak_pixel_dist.get_statistics(show_output=True)
+    redshift_stats = redshift_dist.get_statistics(show_output=True)