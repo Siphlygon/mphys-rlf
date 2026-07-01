ff13e48c18315681480910bc149002accaf6018c
Siphlygon <amparr83@gmail.com>
2026-07-01 18:15:23 +0100

    Great refactor: dataset_prep

--- src/dataset_prep/dataset_h5_to_fits.py
+++ src/dataset_prep/dataset_h5_to_fits.py
@@ -1,27 +1,31 @@
-# This file was created by Ashley and Luna and defines functions neccesary to convert the LOFAR h5 dataset
-# that comes with the project into FITS files needed for PyBDSF, as well as a more specific function which implements
-# the first function and checks for compliance with the arguments, and a main function which uses the config file
-
-import sys
-from astropy.io import fits
-from astropy.io.fits import ImageHDU
-import numpy as np
-import matplotlib.pyplot as plt
-import h5py
-import math
-from pathlib import Path, PurePath
-import shutil
-import utils.paths as pth
-from tqdm import tqdm
-from sklearn.preprocessing import PowerTransformer
-from analysis.image_analyzer import ImageAnalyzer
-import configparser
+"""
+This file was created by Ashley and Luna and defines functions neccesary to convert the LOFAR h5 dataset that comes with
+the project into FITS files needed for PyBDSF, as well as a more specific function which implements the first function
+and checks for compliance with the arguments, and a main function which uses the config file
+"""
 import argparse
+import configparser
+import math
+import shutil
+from pathlib import Path, PurePath
 
-def convert_LOFAR_h5_to_fits( lofar_data_h5: Path, subdir: str, cutoff: int | None, bin_size: int | None ):
+import h5py
+import numpy as np
+from sklearn.preprocessing import PowerTransformer
+from tqdm import tqdm
+
+import utils.paths as pth
+from analysis.image_analyzer import ImageAnalyzer
+
+
+def convert_lofar_h5_to_fits( lofar_data_h5: Path,
+                             subdir: str,
+                             cutoff: int | None,
+                             bin_size: int | None ):
     """
-    A method to convert the LOFAR h5 dataset used in this project into fits files. Outputs fits files to 
-    fits_output_dir, and groups the images into directories by descending sizes.
+    A method to convert the LOFAR h5 dataset used in this project into fits files. Outputs fits files to
+    fits_output_dir, and groups the images into directories by descending sizes. Saves the converted images to the
+    subdirectory of the fits_output_dir specified by subdir.
 
     Parameters
     ----------
@@ -37,8 +41,8 @@ def convert_LOFAR_h5_to_fits( lofar_data_h5: Path, subdir: str, cutoff: int | No
     dataset_analyzer = ImageAnalyzer( subdir )
 
     with h5py.File( str( lofar_data_h5 ), 'r' ) as h5:
-        images = h5[ 'images' ]
-    
+        images : np.ndarray = h5[ 'images' ]  # type: ignore
+
         images_len = images.shape[ 0 ]
         num_to_convert = min( cutoff, images_len ) if cutoff is not None else images_len
 
@@ -57,7 +61,7 @@ def convert_LOFAR_h5_to_fits( lofar_data_h5: Path, subdir: str, cutoff: int | No
             if im_min < 0:
                 raise ValueError( "Images not preprocessed to remove negative values" )
             image = ( image - im_min ) / ( im_max - im_min )
-            
+
             flux_scaled = pt.transform( np.array( [ im_max ] ).reshape(-1, 1) )[ 0, 0 ]
 
             # bin the images based on bin_sizes
@@ -66,9 +70,12 @@ def convert_LOFAR_h5_to_fits( lofar_data_h5: Path, subdir: str, cutoff: int | No
             upper_bound = int( math.ceil( ( i + 1 ) / bin_size ) * bin_size ) - 1
             postfix = postfix / f"{lower_bound}-{upper_bound}"
             postfix = postfix / f"image{i}.fits"
-            dataset_analyzer.save_image_to_FITS( image, postfix, FXSCLD=flux_scaled )
+            dataset_analyzer.save_image_to_fits( image, postfix, FXSCLD=flux_scaled )
 
-def validate_LOFAR_fits_images( subdir: str, cutoff: int | None, bin_size: int | None ):
+
+def validate_lofar_fits_images( subdir: str,
+                               cutoff: int | None,
+                               bin_size: int | None ):
     """
     Ensure FITS images from LOFAR exist in accordance with paths laid out in utils.paths
 
@@ -84,51 +91,58 @@ def validate_LOFAR_fits_images( subdir: str, cutoff: int | None, bin_size: int |
         Directory bins to sort the images into for ease of use. Compliance with the bin structure is enforced.
     """
     fits_dataset_folder = pth.FITS_PARENT / subdir
-    if fits_dataset_folder.exists():
-        #first check if the bin structure is the same or if it has changed
-        for bin in fits_dataset_folder.glob( "*" ):
-            #if bin_size is None, we can check for compliance immediately - if we have a directory, we need to clean the dir
-            if bin_size is None:
-                if bin.is_dir():
-                    print( "Directory not compliant with bin_size of None" )
-                    shutil.rmtree( fits_dataset_folder )
-                break
 
-            lower_bound, upper_bound = bin.name.split( '-' )
-            lower_bound, upper_bound = int( lower_bound ), int( upper_bound )
-            if lower_bound % bin_size == 0 and upper_bound % bin_size == bin_size - 1:
-                continue
-            else:
-                print( f"Bin {lower_bound}-{upper_bound} ({bin.parent/bin.name}) not compliant with bin size {bin_size}, removing directory..." )
+    # For now, if the fits dataset folder doesn't exist, we just return.
+    if not fits_dataset_folder.exists():
+        return
+
+    #first check if the bin structure is the same or if it has changed
+    for bin in fits_dataset_folder.glob( "*" ):
+        #if bin_size is None, we can check for compliance immediately - if we have a directory, we need to clean the dir
+        if bin_size is None:
+            if bin.is_dir():
+                print( "Directory not compliant with bin_size of None" )
                 shutil.rmtree( fits_dataset_folder )
-                break
+            break
 
-        #then check we're not at or above the cutoff if it exists
-        if cutoff is not None:
-            num_files = sum( 1 for _ in fits_dataset_folder.rglob( "*.fits" ) )
-            print( f'found {num_files}, cutoff {cutoff}' )
-            #if we have more than cutoff, we should delete the dir and regenerate to make sure we have exactly cutoff
-            if num_files > cutoff:
-                shutil.rmtree( fits_dataset_folder )
-            elif num_files == cutoff:
-                return
+        lower_bound, upper_bound = bin.name.split( '-' )
+        lower_bound, upper_bound = int( lower_bound ), int( upper_bound )
+        if lower_bound % bin_size == 0 and upper_bound % bin_size == bin_size - 1:
+            continue
 
-    convert_LOFAR_h5_to_fits( pth.LOFAR_DATA_PATH, subdir, cutoff, bin_size )
+        print( f"Bin {lower_bound}-{upper_bound} ({bin.parent/bin.name}) not compliant with bin size {bin_size},"
+                " removing directory..." )
+        shutil.rmtree( fits_dataset_folder )
+        break
+
+    #then check we're not at or above the cutoff if it exists
+    if cutoff is not None:
+        num_files = sum( 1 for _ in fits_dataset_folder.rglob( "*.fits" ) )
+        print( f'found {num_files}, cutoff {cutoff}' )
+        #if we have more than cutoff, we should delete the dir and regenerate to make sure we have exactly cutoff
+        if num_files > cutoff:
+            shutil.rmtree( fits_dataset_folder )
+        elif num_files == cutoff:
+            return
+
+    convert_lofar_h5_to_fits( pth.LOFAR_DATA_PATH, subdir, cutoff, bin_size )
 
 
 
 if __name__ == "__main__":
     # allow for bin sizes to be specified manually as command-line arguments, or use config if nothing specified
     parser = argparse.ArgumentParser()
-    parser.add_argument( "--config", help=f"Which config to use for image generation, as defined in {pth.PROGRAM_CONFIG.name}", type=str )
+    parser.add_argument( "--config",
+                        help=f"Which config to use for image generation, as defined in " f"{pth.PROGRAM_CONFIG.name}",
+                        type=str )
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
     config.read( pth.PROGRAM_CONFIG )
-    specific_config = config[ args.config ]
+    config = config[ args.config ]
 
-    cutoff = int( specific_config[ 'VM_FITS_COUNT_CUTOFF' ] ) if specific_config[ 'VM_FITS_COUNT_CUTOFF' ] != 'None' else None
-    bin_size = int( specific_config[ 'FOLDER_SIZE' ] )
-    vm_dataset_subdir = specific_config[ 'DATASET_SUBDIR' ]
+    cutoff = int( config[ 'VM_FITS_COUNT_CUTOFF' ] ) if config[ 'VM_FITS_COUNT_CUTOFF' ] != 'None' else None
+    bin_size = int( config[ 'FOLDER_SIZE' ] )
+    vm_dataset_subdir = config[ 'DATASET_SUBDIR' ]
 
-    validate_LOFAR_fits_images( vm_dataset_subdir, cutoff, bin_size )
+    validate_lofar_fits_images( vm_dataset_subdir, cutoff, bin_size )
--- src/dataset_prep/save_dataset_maxvals.py
+++ src/dataset_prep/save_dataset_maxvals.py
@@ -1,15 +1,19 @@
+import argparse
+import configparser
 from pathlib import Path
+
 import h5py
 import numpy as np
+
 import utils.paths as pth
-import configparser
-import argparse
+
 
 def write_maxvals_of_h5_to_file( outfile: Path, infile: Path ):
     """
-    A function to go through an input h5 file, select the 'images' category, and sum along axes 1 and 2 to get an array of the maximum
-    pixel values of each image, then save the numpy array to an output file. This saves the maximum pixel values neccesary for the box-cox
-    power transform in a much more portable format, shrinking the file size by 6400x and allowing it to be copied for access by multiple nodes.
+    A function to go through an input h5 file, select the 'images' category, and sum along axes 1 and 2 to get an array
+    of the maximum pixel values of each image, then save the numpy array to an output file. This saves the maximum pixel
+    values neccesary for the box-cox power transform in a much more portable format, shrinking the file size by 6400x
+    and allowing it to be copied for access by multiple nodes.
 
     Parameters
     ----------
@@ -22,9 +26,12 @@ def write_maxvals_of_h5_to_file( outfile: Path, infile: Path ):
         max_vals = np.max( f[ "images" ][ : ], axis=(1, 2) )
     np.save( outfile, max_vals )
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument( "--config", help=f"Which config to use for obtaining training dataset maxvals, as defined in {pth.PROGRAM_CONFIG.name}", type=str )
+    parser.add_argument( "--config",
+                        help=f"Which config to use for obtaining training dataset maxvals, as defined in "
+                        f"{pth.PROGRAM_CONFIG.name}", type=str )
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
@@ -37,5 +44,5 @@ if __name__ == "__main__":
 
     # if train_data_path is pth.LOFAR_DATA_PATH, we also want to make sure to update the dataset subdir
     write_maxvals_of_h5_to_file( pth.NP_ARRAY_PARENT / 'dataset' / pth.MAXVALS, train_data_path )
-
-    write_maxvals_of_h5_to_file( pth.NP_ARRAY_PARENT / specific_config[ 'generated_subdir' ] / pth.MAXVALS, train_data_path )
+    write_maxvals_of_h5_to_file( pth.NP_ARRAY_PARENT / specific_config[ 'generated_subdir' ] / pth.MAXVALS,
+                                train_data_path )
