ff989298276b31d795de1e4531235877ee2ee48e
Siphlygon <amparr83@gmail.com>
2026-07-01 20:39:01 +0100

    Great refactor: hardcastle_catalogue

--- hardcastle_catalogue/hardcastle_catalogue.py
+++ hardcastle_catalogue/hardcastle_catalogue.py
@@ -1,14 +1,16 @@
-from astropy.io import fits
 import logging
-from tqdm import tqdm
-import numpy as np
+import os
 from enum import Enum
 from pathlib import Path
-import os
-import requests
 
-import utils.paths as pths
+import numpy as np
+import requests
+from astropy.io import fits
+from tqdm import tqdm
+
 import utils.logging
+import utils.paths as pths
+
 
 class Source(Enum):
     """
@@ -36,6 +38,22 @@ class HardcastleCatalogue:
     def __init__(self,
                  resolved_only: bool = True,
                  cat: str = "hardcastle2023"):
+        """
+        Initialises the HardcastleCatalogue class by loading the specified Hardcastle catalogue and filtering for
+        resolved sources if specified.
+
+        Parameters
+        ----------
+        resolved_only : bool, optional
+            Whether to only consider resolved sources, by default True
+        cat : str, optional
+            The catalogue to use, by default "hardcastle2023"
+
+        Raises
+        ------
+        NotImplementedError
+            If an unsupported catalogue is specified
+        """
         self.logger = utils.logging.get_logger("HardcastleCatalogue", logging.DEBUG)
 
         # Option to only consider resolved sources or not
@@ -48,10 +66,10 @@ class HardcastleCatalogue:
             case "hardcastle2023":
                 self.catalogue_data = self.load_hardcastle_catalogue()
             case "hardcastle2025":
-                # todo: need to make sure this is downloaded first; maybe add a database downloading script?
                 self.catalogue_data = self.load_hardcastle_catalogue(cat, pths.DATASET_PARENT / "agn-v1.1.fits")
             case _:
-                raise ValueError("Invalid catalogue")
+                raise NotImplementedError("Invalid catalogue")
+
 
     def download_hardcastle_catalogue(self,
                                       cat : str = "hardcastle2023",
@@ -59,7 +77,18 @@ class HardcastleCatalogue:
         """
         Downloads a Hardcastle catalogue FITS file from the LOFAR website if it does not already exist.
 
-        :param save_path: The path to save the downloaded FITS file.
+        Parameters
+        ----------
+        cat : str, optional
+            The catalogue to use, by default "hardcastle2023"
+        save_path : Path, optional
+            The path to save the downloaded FITS file, by default
+            pths.INITIAL_DATASET/"combined-release-v1.2-LM_opt_mass.fits"
+
+        Raises
+        ------
+        NotImplementedError
+            If an unsupported catalogue is specified
         """
         if os.path.exists(save_path):
             self.logger.info(f'Hardcastle catalogue already exists at {save_path}. Skipping download.')
@@ -73,7 +102,7 @@ class HardcastleCatalogue:
             case "hardcastle2025":
                 url = "https://lofar-surveys.org/public/DR2/AGN_selection/agn-v1.1.fits"
             case _:
-                raise ValueError("Invalid catalogue")
+                raise NotImplementedError("Invalid catalogue")
         self.logger.info(f'Downloading Hardcastle catalogue from {url}. This will take a while...')
         response = requests.get(url, stream=True)
 
@@ -85,16 +114,28 @@ class HardcastleCatalogue:
         else:
             self.logger.error(f'Failed to download Hardcastle catalogue. Status code: {response.status_code}')
 
+
     def load_hardcastle_catalogue(self,
                                   cat : str = "hardcastle2023",
-                                  file_path : Path = pths.INITIAL_DATASET / "combined-release-v1.2-LM_opt_mass.fits")\
-            -> list[tuple]:
+                                  file_path : Path = pths.INITIAL_DATASET / "combined-release-v1.2-LM_opt_mass.fits"
+                                  ) -> list[tuple]:
         """
-        Loads the Hardcastle catalogue from a FITS file and filters for resolved items. This turns the ~4.1mil items from the
-        LoTSS-DR2 release w/ optical sources to 314,769 values. Note that this does not get pixel value for the images.
+        Loads the Hardcastle catalogue from a FITS file and filters for resolved items. This turns the ~4.1mil items
+        from the LoTSS-DR2 release w/ optical sources to 314,769 values. Note that this does not get pixel value for the
+        images.
 
-        :param file_path: The path to the Hardcastle catalogue FITS file.
-        :return: A list of resolved items from the catalogue.
+        Parameters
+        ----------
+        cat : str, optional
+            The catalogue to use, by default "hardcastle2023"
+        file_path : Path, optional
+            The path to the FITS file containing the Hardcastle catalogue, by default
+            pths.INITIAL_DATASET / "combined-release-v1.2-LM_opt_mass.fits"
+            
+        Returns
+        -------
+        list[tuple]
+            A list of tuples containing the data for each resolved item in the Hardcastle catalogue.
         """
         self.download_hardcastle_catalogue(cat, save_path=file_path)
         self.logger.info(f"Loading Hardcastle catalogue from {file_path}")
@@ -103,22 +144,26 @@ class HardcastleCatalogue:
                 catalogue_data = hdul[1].data
         except Exception as e:
             self.logger.error(f"Error loading Catalogue file: {e}.")
-            raise Exception("Error loading Catalogue file")
+            raise Exception("Error loading Catalogue file") from e
 
         # Get the headers of resolved sources
         if self.resolved_only:
-            catalogue_data = catalogue_data[catalogue_data['Resolved'] == True]
+            catalogue_data = catalogue_data[catalogue_data['Resolved']]
             self.logger.info(f"Loaded {len(catalogue_data)} resolved items from the Hardcastle catalogue.")
         else:
-            self.logger.info(f"Loaded {len(catalogue_data)} items from the Hardcastle catalogue (including unresolved).")
+            self.logger.info(f"Loaded {len(catalogue_data)} total items from the Hardcastle catalogue.")
 
         return catalogue_data
+
 
     def get_positions(self) -> list[tuple[float, float]]:
         """
         Extracts the positions (RA, DEC) from the resolved items in the Hardcastle catalogue.
 
-        :return: A list of tuples containing (RA, DEC) for each resolved item.
+        Returns
+        -------
+        list[tuple[float, float]]
+            A list of tuples containing the RA and DEC positions of resolved sources.
         """
         positions = []
         for item in tqdm(self.catalogue_data, desc="Extracting positions..."):
@@ -127,24 +172,41 @@ class HardcastleCatalogue:
             positions.append((ra, dec))
         return positions
 
+
     def get_values(self,
                    value : Source | str) -> list:
         """
         Extracts a specific value from the resolved items in the Hardcastle catalogue.
 
-        :param value: The name of the value to extract (e.g., 'RA', 'DEC', 'Total_flux').
-        :return: A list of the specified value for each resolved item.
+        Parameters
+        ----------
+        value : Source | str
+            The name of the value to extract (e.g., 'RA', 'DEC', 'Total_flux').
+
+        Returns
+        -------
+        list
+            A list of the specified value for each resolved item.
         """
         key = value.value if isinstance(value, Source) else value
         return self.catalogue_data[key]
+
 
     def get_multiple_values(self,
                             *args : Source | str) -> np.ndarray:
         """
         Extracts multiple specified values from the resolved items in the Hardcastle catalogue.
 
-        :param args: The names of the values to extract (e.g., 'RA', 'DEC', 'Flux').
-        :return: A 2D numpy array where each column corresponds to one of the specified values and each row correspondsto a resolved item.
+        Parameters
+        ----------
+        *args : Source | str
+            The names of the values to extract (e.g., 'RA', 'DEC', 'Flux').
+
+        Returns
+        -------
+        np.ndarray
+            A 2D numpy array where each column corresponds to one of the specified values and each row correspondsto a
+            resolved item.
         """
         # Account for the ability to input enum e.g., Source.RA instead of "RA"
         keys = [arg.value if isinstance(arg, Source) else arg for arg in args]
