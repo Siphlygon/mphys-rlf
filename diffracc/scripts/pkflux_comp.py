3a281246dc112faf866275ee7a30f5ddf52d4d98
Siphlygon <amparr83@gmail.com>
2026-07-12 21:15:43 +0100

    Refactor/add argparser to existing scripts

--- diffracc/scripts/pkflux_comp.py
+++ diffracc/scripts/pkflux_comp.py
@@ -1,3 +1,5 @@
+import argparse
+
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
@@ -5,34 +7,56 @@ from astropy.io import fits
 from ..utils import paths
 from ..utils.power_transform import PeakFluxPowerTransformer
 
-img_directory = paths.FITS_PARENT / 'retrained_loguniform'
-power_transformer = PeakFluxPowerTransformer( 'retrained_loguniform' )
 
-peak_fluxes_conditioned = []
-peak_fluxes_from_imgs = []
+def main(args):
+    """
+    Main function to compare conditioned peak fluxes with image peak fluxes.
 
-for img_path in img_directory.rglob( "*.fits" ):
-    with fits.open( img_path ) as hdul:
-        flux_scaled = hdul[ 0 ].header[ 'FXSCLD' ]
-        peak_flux_from_img = np.max( hdul[ 0 ].data )
-    peak_flux_conditioned = power_transformer.inverse_transform( np.array( [ flux_scaled ] ) )
+    Parameters
+    ----------
+    args : argparse.Namespace
+        Command-line arguments containing the subdirectory path.
+    """
+    img_directory = paths.FITS_PARENT / args.subdir
+    power_transformer = PeakFluxPowerTransformer(args.subdir)
 
-    peak_fluxes_from_imgs.append( peak_flux_from_img )
-    peak_fluxes_conditioned.append( peak_flux_conditioned )
+    peak_fluxes_conditioned = []
+    peak_fluxes_from_imgs = []
 
-plt.scatter( peak_fluxes_conditioned, peak_fluxes_from_imgs, s=0.01 )
+    for img_path in img_directory.rglob("*.fits"):
+        with fits.open(img_path) as hdul:
+            flux_scaled = hdul[0].header['FXSCLD']
+            peak_flux_from_img = np.max(hdul[0].data)
+        peak_flux_conditioned = power_transformer.inverse_transform(np.array([flux_scaled]))
 
-min_peakflux = min( np.min( peak_fluxes_conditioned ), np.min( peak_fluxes_from_imgs ) ) if len( peak_fluxes_from_imgs ) > 0 else np.min( peak_fluxes_conditioned )
-max_peakflux = max( np.max( peak_fluxes_conditioned ), np.max( peak_fluxes_from_imgs ) ) if len( peak_fluxes_from_imgs ) > 0 else np.max( peak_fluxes_conditioned )
+        peak_fluxes_from_imgs.append(peak_flux_from_img)
+        peak_fluxes_conditioned.append(peak_flux_conditioned)
 
-x = np.geomspace( min_peakflux, max_peakflux )
-plt.plot( x, x, color='r' )
-plt.grid()
+    plt.scatter(peak_fluxes_conditioned, peak_fluxes_from_imgs, s=0.01)
 
-plt.title( 'Peak flux from conditioning vs image' )
-plt.xlabel( 'Conditioned peak flux, Jy/beam' )
-plt.ylabel( 'Image peak flux, Jy/beam' )
-plt.xscale( 'log' )
-plt.yscale( 'log' )
-plt.savefig( 'peak_flux_conditioning.png' )
-plt.show()
+    if len(peak_fluxes_from_imgs) > 0:
+        min_peakflux = min(np.min(peak_fluxes_conditioned), np.min(peak_fluxes_from_imgs))
+        max_peakflux = max(np.max(peak_fluxes_conditioned), np.max(peak_fluxes_from_imgs))
+    else:
+        min_peakflux = np.min(peak_fluxes_conditioned)
+        max_peakflux = np.max(peak_fluxes_conditioned)
+
+    x = np.geomspace(min_peakflux, max_peakflux)
+    plt.plot(x, x, color='r')
+    plt.grid()
+
+    plt.title('Peak flux from conditioning vs image')
+    plt.xlabel('Conditioned peak flux, Jy/beam')
+    plt.ylabel('Image peak flux, Jy/beam')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.savefig('peak_flux_conditioning.png')
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compare conditioned peak fluxes with image peak fluxes.")
+    parser.add_argument('--subdir', type=str, required=True, help="Subdirectory containing FITS files.")
+    args = parser.parse_args()
+
+    main(args)
--- diffracc/scripts/size_comp.py
+++ diffracc/scripts/size_comp.py
@@ -1,38 +1,48 @@
+import argparse
+
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
 
 from ..utils import paths
-from ..utils.power_transform import PeakFluxPowerTransformer
 
-img_directory = paths.FITS_PARENT / 'snr15_loguniform'
-power_transformer = PeakFluxPowerTransformer( 'snr15_loguniform' )
 
-lass = []
-lass_from_img = []
+def main(args):
+    img_directory = paths.FITS_PARENT / args.subdir
 
-for img_path in img_directory.rglob( "*.fits" ):
-    with fits.open( img_path ) as hdul:
-        las = hdul[ 0 ].header[ 'LASIZE' ]
-        img = hdul[ 0 ].data
-        # not actually largest angular size, just a basic proxy with no physical motivation
-        las_from_img = np.sqrt( (img > np.median( img ) + 3 * np.std( img )).sum() )
-    lass.append( las )
-    lass_from_img.append( las_from_img )
+    lass = []
+    lass_from_img = []
 
-plt.scatter( lass, lass_from_img, s=0.01 )
+    for img_path in img_directory.rglob("*.fits"):
+        with fits.open(img_path) as hdul:
+            las = hdul[0].header['LASIZE']
+            img = hdul[0].data
+            # not actually largest angular size, just a basic proxy with no physical motivation
+            las_from_img = np.sqrt((img > np.median(img) + 3 * np.std(img)).sum())
+        lass.append(las)
+        lass_from_img.append(las_from_img)
 
-minval = min( np.min( lass ), np.min( lass_from_img ) ) if len( lass ) > 0 else np.min( lass )
-maxval = max( np.max( lass ), np.max( lass_from_img ) ) if len( lass ) > 0 else np.max( lass )
+    plt.scatter(lass, lass_from_img, s=0.01)
 
-x = np.geomspace( minval, maxval )
-plt.plot( x, x, color='r' )
-plt.grid()
+    minval = min(np.min(lass), np.min(lass_from_img)) if len(lass) > 0 else np.min(lass)
+    maxval = max(np.max(lass), np.max(lass_from_img)) if len(lass) > 0 else np.max(lass)
 
-plt.title( 'Size conditioning test' )
-plt.xlabel( 'Largest Angular Size (arcsec)' )
-plt.ylabel( 'Random Size Proxy (a.u.)' )
-plt.xscale( 'log' )
-plt.yscale( 'log' )
-plt.savefig( 'size_comp.png' )
-plt.show()
+    x = np.geomspace(minval, maxval)
+    plt.plot(x, x, color='r')
+    plt.grid()
+
+    plt.title('Size conditioning test')
+    plt.xlabel('Largest Angular Size (arcsec)')
+    plt.ylabel('Random Size Proxy (a.u.)')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.savefig('size_comp.png')
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compare conditioned peak fluxes with image peak fluxes.")
+    parser.add_argument('--subdir', type=str, required=True, help="Subdirectory containing FITS files.")
+    args = parser.parse_args()
+
+    main(args)
